df7f48750ea00236585779f181b442106f25299d
Daniel Morales <daniel.a.moralesalt@gmail.com>
2020-01-22 00:08:46 -0500

    Step 3 implemented. Dark mode toggle working. MVP metgit add .!

--- src/hooks/useDarkMode.jsx
+++ src/hooks/useDarkMode.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import useLocalStorage from "./useLocalStorage";
+import { useLocalStorage } from "./useLocalStorage";
 
 export const useDarkMode = () => {
 	const [darkMode, setDarkMode] = useLocalStorage("darkMode");
